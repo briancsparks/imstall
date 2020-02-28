b39cc3f4c8e1abf73924362b56e3eabfdd1ea8c6
Brian C Sparks <briancsparks@gmail.com>
2020-02-28 10:59:32 +0000

    wip

--- imstall.js
+++ imstall.js
@@ -5,44 +5,80 @@ const os                  = require('os');
 const path                = require('path');
 const { spawn, execSync } = require('child_process');
 
-//console.error(`No recipe for "${process.argv[2]}"`);
+//if (require.module === module) {
+  main();
+//}
 
-var args            = Array.from(process.argv);
-var npxName         = args.shift();
-var imstallName     = args.shift();
-var commandName     = args.shift();
+function main() {
 
-var distro          = 'mac';
-if (os.platform() === 'linux') {
-  distro            = execSync('lsb_release -is');
+  //console.error(`No recipe for "${process.argv[2]}"`);
+
+  var args            = Array.from(process.argv);
+  var npxName         = args.shift();
+  var imstallName     = args.shift();
+  var commandName     = args.shift();
+
+  var distro          = 'mac';
+  if (os.platform() === 'linux') {
+    distro            = ''+ execSync('lsb_release -is');
+  }
+  distro = distro.trim();
+
+  // Is this an apt-get install?
+  if (commandName === 'agu') {
+    var command = path.join(process.cwd(), 'lib', commandName);
+    return spawnIt(commandName, command, args, function(code) {
+      return;
+    });
+  }
+
+  var command = path.join(process.cwd(), 'lib', distro, commandName);
+
+  var success = false;
+  if (fs.existsSync(command)) {
+    //console.log(`clog ${command}`, args);
+
+    const proc = spawn(command, args);
+
+    proc.stdout.on('data', (data) => {
+      //console.log(`stdout: ${data}`);
+      console.log(''+ data);
+    });
+
+    proc.stderr.on('data', (data) => {
+      //console.error(`stderr: ${data}`);
+      console.error(''+ data);
+    });
+
+    proc.on('close', (code) => {
+      console.log(`imstall ${commandName} exited with code ${code}`);
+    });
+
+  } else {
+    console.log(`Cannot find command: ${command}`);
+  }
+
+  //if (!success) {
+  //  console.error(`Failed to run: ${command}`);
+  //}
 }
 
-var command = path.join(process.cwd(), 'lib', distro, commandName);
-
-console.log(command, args);
-
-var success = false;
-if (fs.existsSync(command)) {
-  console.log(`clog ${command}`, args);
-
+function spawnIt(commandName, command, args, callback) {
   const proc = spawn(command, args);
 
   proc.stdout.on('data', (data) => {
     //console.log(`stdout: ${data}`);
-    console.log(data);
+    console.log(''+ data);
   });
 
   proc.stderr.on('data', (data) => {
     //console.error(`stderr: ${data}`);
-    console.error(data);
+    console.error(''+ data);
   });
 
   proc.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
+    console.log(`imstall ${commandName} exited with code ${code}`);
+    callback(code);
   });
-}
-
-if (!success) {
-  console.error(`Failed to run: ${command}`);
 }
 
