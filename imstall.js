12f584c82a7c893a07b73420e9c3d1158dfc7382
Brian C Sparks <brian.c.sparks@hp.com>
2020-02-28 01:39:42 -0800

    WIP

--- imstall.js
+++ imstall.js
@@ -1,4 +1,48 @@
 #!/usr/bin/env node
 
-console.error(`No recipe for "${process.argv[2]}"`);
+const fs                  = require('fs');
+const os                  = require('os');
+const path                = require('path');
+const { spawn, execSync } = require('child_process');
+
+//console.error(`No recipe for "${process.argv[2]}"`);
+
+var args            = Array.from(process.argv);
+var npxName         = args.shift();
+var imstallName     = args.shift();
+var commandName     = args.shift();
+
+var distro          = 'mac';
+if (os.platform() === 'linux') {
+  distro            = execSync('lsb_release -is');
+}
+
+var command = path.join(process.cwd(), 'lib', distro, commandName);
+
+console.log(command, args);
+
+var success = false;
+if (fs.existsSync(command)) {
+  console.log(`clog ${command}`, args);
+
+  const proc = spawn(command, args);
+
+  proc.stdout.on('data', (data) => {
+    //console.log(`stdout: ${data}`);
+    console.log(data);
+  });
+
+  proc.stderr.on('data', (data) => {
+    //console.error(`stderr: ${data}`);
+    console.error(data);
+  });
+
+  proc.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+}
+
+if (!success) {
+  console.error(`Failed to run: ${command}`);
+}
 
